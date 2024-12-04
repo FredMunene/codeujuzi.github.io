2585b99472cb2f36c2e49f7afe38f7431c452937
FredMunene <mfredgitonga@gmail.com>
2024-12-04 17:28:24 +0300

    refactor signup, signin handler - split login and sign in - split signup and register

--- http/handlers.go
+++ http/handlers.go
@@ -1,16 +1,12 @@
 package handlers
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"text/template"
-
-	"codeujuzi.github.io/services"
 )
 
-func HomePageHandler(w http.ResponseWriter, r *http.Request){
-
+func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("template/index.html")
 	if err != nil {
 		internalServerErrorHandler(w)
@@ -24,11 +20,9 @@ func HomePageHandler(w http.ResponseWriter, r *http.Request){
 		log.Println("Index.html template parsing error: ", err)
 		return
 	}
-	
-
 }
 
-func SigninHandler(w http.ResponseWriter, r *http.Request){
+func SigninHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("template/login.html")
 	if err != nil {
 		internalServerErrorHandler(w)
@@ -42,30 +36,10 @@ func SigninHandler(w http.ResponseWriter, r *http.Request){
 		log.Println("Index.html template parsing error: ", err)
 		return
 	}
-
 }
 
-func LoginHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method == http.MethodPost {
-		err := r.ParseForm()
-		if err != nil {
-			fmt.Println("unable to parse request")
-		}
-
-		email := r.FormValue("email")
-		passwd := r.FormValue("password")
-
-		fmt.Println(email,":",passwd)
-		fmt.Fprintf(w,"%v:%v",email,passwd)
-
-		hashedPasswd := services.GetHashedPasswd(passwd)
-
-		fmt.Printf("%x",hashedPasswd)
-	}
-}
-
-func RegisterHandler(w http.ResponseWriter, r *http.Request){
-	tmpl, err := template.ParseFiles("/template/register.html")
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("template/register.html")
 	if err != nil {
 		internalServerErrorHandler(w)
 		log.Println("Index.html parsing error: ", err)
@@ -78,10 +52,9 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request){
 		log.Println("Index.html template parsing error: ", err)
 		return
 	}
-
 }
 
-func LessonsHandler(w http.ResponseWriter, r *http.Request){
+func LessonsHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("template/lessons.html")
 	if err != nil {
 		internalServerErrorHandler(w)
@@ -95,9 +68,9 @@ func LessonsHandler(w http.ResponseWriter, r *http.Request){
 		log.Println("Index.html template parsing error: ", err)
 		return
 	}
-
 }
-func ProfileHandler(w http.ResponseWriter, r *http.Request){
+
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("template/profile.html")
 	if err != nil {
 		internalServerErrorHandler(w)
@@ -111,10 +84,9 @@ func ProfileHandler(w http.ResponseWriter, r *http.Request){
 		log.Println("Index.html template parsing error: ", err)
 		return
 	}
-
 }
 
-func CourseHandler(w http.ResponseWriter, r *http.Request){
+func CourseHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("template/course-outline.html")
 	if err != nil {
 		internalServerErrorHandler(w)
@@ -128,32 +100,33 @@ func CourseHandler(w http.ResponseWriter, r *http.Request){
 		log.Println("Index.html template parsing error: ", err)
 		return
 	}
-
 }
 
-func LogoutHandler(w http.ResponseWriter, r *http.Request){
-	http.Redirect(w,r," http://localhost:8000/",http.StatusSeeOther)
-
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, " http://localhost:8000/", http.StatusSeeOther)
 }
 
 // func TranslateHandler(w http.ResponseWriter, r *http.Request){
 
 // }
 
-func internalServerErrorHandler(w http.ResponseWriter){
-	renderErrorPage(w,http.StatusInternalServerError,"Permission denied")
+func internalServerErrorHandler(w http.ResponseWriter) {
+	renderErrorPage(w, http.StatusInternalServerError, "Permission denied")
 }
-	
-func wrongMethodErrorHandler(w http.ResponseWriter){
-	renderErrorPage(w,http.StatusMethodNotAllowed,"Permission denied")
+
+func wrongMethodErrorHandler(w http.ResponseWriter) {
+	renderErrorPage(w, http.StatusMethodNotAllowed, "Permission denied")
 }
-func pageNotFoundErrorHandler(w http.ResponseWriter){
-	renderErrorPage(w,http.StatusNotFound,"The page you are looking for does not exist")
+
+func pageNotFoundErrorHandler(w http.ResponseWriter) {
+	renderErrorPage(w, http.StatusNotFound, "The page you are looking for does not exist")
 }
+
 func badRequestHandler(w http.ResponseWriter) {
 	renderErrorPage(w, http.StatusBadRequest, " Try the home page")
 }
-func renderErrorPage(w http.ResponseWriter, statusCode int, message string){
+
+func renderErrorPage(w http.ResponseWriter, statusCode int, message string) {
 	w.WriteHeader(statusCode)
 	tmpl, err := template.ParseFiles("template/error.html")
 	if err != nil {
