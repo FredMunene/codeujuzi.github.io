757af0f59ab1ac068f0c5125b1b70ec4d4ee5e4b
FredMunene <mfredgitonga@gmail.com>
2024-12-04 17:28:48 +0300

    refactor password hashing to use bycrypt

--- services/passwd.go
+++ services/passwd.go
@@ -1,14 +1,28 @@
 package services
 
 import (
-	"crypto/sha256"
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-func GetHashedPasswd(passwd string) string {
-	hasher := sha256.New()
-	hasher.Write([]byte(passwd))
-	fmt.Printf("%x", hasher.Sum(nil))
+func GetHashedPasswd(passwd string) (string, error) {
+	// hasher := sha256.New()
+	// hasher.Write([]byte(passwd))
 
-	return string(hasher.Sum(nil))
+	hash, err := bcrypt.GenerateFromPassword([]byte(passwd), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %v", err)
+	}
+
+	return string(hash), nil
+}
+
+func ComparePasswds(dbPasswd, userPasswd string) bool {
+	hashedPasswd, err := GetHashedPasswd(userPasswd)
+	if err != nil {
+		return false
+	}
+	fmt.Printf("%v,\n%v",dbPasswd,hashedPasswd)
+	return (hashedPasswd == dbPasswd)
 }
